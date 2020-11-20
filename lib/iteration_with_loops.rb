612dcd8684b4a6c1c7634d9fc51bb7bed8875d0a
Nate Croteau <natekcroteau@gmail.com>
2020-11-20 20:52:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ all_the_strings = []
 
   while outer_index < src.count do 
     inner_index = 0 
-    while inner_index < src[outer_index][inner_index].count do
+    while inner_index < src[outer_index].count do
       if src[outer_index][inner_index].string?
         all_the_strings << src[outer_index][inner_index]
       end
